751f814f30d04521f146fb2f17bad54ff5485085
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-03-12 17:14:12 -0500

    Delete Ejercicios_POO_en_JAVA/_ejercicio_04_Figuras_Geometricas directory

--- Ejercicios_POO_en_JAVA/_ejercicio_04_Figuras_Geometricas/src/cuadrado.java
+++
@@ -1,9 +0,0 @@
-package ejercicio_04;
-
-public class cuadrado extends rectangulo {
-
-    public cuadrado(double lado){
-        super(lado,lado);
-    }
-    //no es necesario sobre escribir calcularArea porque la implementar de rectangulo ya estamos obteniendo la funcionalidad
-}
--- Ejercicios_POO_en_JAVA/_ejercicio_04_Figuras_Geometricas/src/figura.java
+++
@@ -1,5 +0,0 @@
-package ejercicio_04;
-//una calse abstracta es la que les indica a sus clases padre a sus clases hijas cuales funcionalidades deben implementar obligatoriamente.
-public abstract class figura {
-    public abstract  double calcularArea();
-}
