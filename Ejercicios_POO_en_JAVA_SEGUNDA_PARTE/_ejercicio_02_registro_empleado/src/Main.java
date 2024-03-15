b397f3311586d602f1485889e081e9460fde0538
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-03-15 16:28:51 -0500

    Update Main.java

--- Ejercicios_POO_en_JAVA_SEGUNDA_PARTE/_ejercicio_02_registro_empleado/src/Main.java
+++ Ejercicios_POO_en_JAVA_SEGUNDA_PARTE/_ejercicio_02_registro_empleado/src/Main.java
@@ -1,21 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         GestionEmpleados gestionEmpleados = new GestionEmpleados();
 
-        Empleado empleado1 = new EmpleadoPermanente("Armando Casas",78,1,2500,100);
-        Empleado empleado2 = new EmpleadoTemporal("Aquiles Brinco",17,2,800000,"31/02/3033");
+        boolean salir = false;
+        while (!salir) {
+            String opcion = JOptionPane.showInputDialog("selecciona una opcion:\n"
+                    + "1.añadir empleado\n"
+                    + "2.elimianr empleado\n"
+                    + "3.mostrar empleados\n"
+                    + "4.salir");
 
-        gestionEmpleados.agregarEmpleado(empleado1);
-        gestionEmpleados.agregarEmpleado(empleado2);
-
-        gestionEmpleados.mostrarEmpleado();
-
-        gestionEmpleados.eliminarEmpleado(1);
-
-        System.out.println("\n Despues de haberse eliminado");
-        gestionEmpleados.mostrarEmpleado();
-
+            switch (opcion) {
+                case "1":
+                    String nombre = JOptionPane.showInputDialog("ingresa el nombre del empleado:");
+                    int edad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la edad del empleado:"));
+                    int tipo = Integer.parseInt(JOptionPane.showInputDialog("ingrese el tipo de empleado que sera(1 - permanente, 2 - temporal):"));
+                    double salario = Double.parseDouble(JOptionPane.showInputDialog("ingrese el salario del empleado:"));
+                    if (tipo == 1) {
+                        int antiguedad = Integer.parseInt(JOptionPane.showInputDialog("ingrese la antiguedad del empleado permanente:"));
+                        gestionEmpleados.agregarEmpleado(new EmpleadoPermanente(nombre, edad, tipo, salario, antiguedad));
+                    } else if (tipo == 2) {
+                        String fechaFin = JOptionPane.showInputDialog("ingrese la fecha de fin del contrato (dd/mm/yyyy) del empleado temporal:");
+                        gestionEmpleados.agregarEmpleado(new EmpleadoTemporal(nombre, edad, tipo, salario, fechaFin));
+                    } else {
+                        JOptionPane.showMessageDialog(null, "opcion invalida");
+                    }
+                    break;
+                case "2":
+                    int idEliminar = Integer.parseInt(JOptionPane.showInputDialog("ingress el id del empleado para eliminar:"));
+                    gestionEmpleados.eliminarEmpleado(idEliminar);
+                    break;
+                case "3":
+                    gestionEmpleados.mostrarEmpleado();
+                    break;
+                case "4":
+                    salir = true;
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "opcion invalida");
+                    break;
+            }
+        }
     }
 }
